3eeda11f5730e76e50a91bb94ebdf0fd2c8f7944
g00197439 <60353410+g00197439@users.noreply.github.com>
2020-10-01 10:04:10 -0400

    Enemy prefabs created

---
+++ Assets/Scripts/BoundsCheck.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    /// <summary>
+    /// Checks whether a GameObject is on screen and can force it to stay on screen.
+    /// </summary>
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+    public bool keepOnScreen = true;
+
+    [Header("Set Dynamically")]
+    public bool isOnScreen = true;
+    public float camWidth;
+    public float camHeight;
+
+    private void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        isOnScreen = true;
+
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+            isOnScreen = false;
+        }
+        if (pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+            isOnScreen = false;
+        }
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+            isOnScreen = false;
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+            isOnScreen = false;
+        }
+
+        if(keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+        }
+
+        transform.position = pos;
+    }
+
+    // Draw the bounds in the Scene pane using OnDrawGizmos()
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+}
